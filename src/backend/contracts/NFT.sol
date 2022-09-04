2f3ea7f973acb70c3871b10765bea266b8c364b9
unknown <nmanikandan2211@gmail.com>
2022-09-05 00:23:45 +0530

    smart contract and testing finished

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFT is ERC721URIStorage {
     uint256 public tokenCount;
 
-    constructor() ERC721("DApp NFT", "DApp") {}
+    constructor() ERC721("DApp NFT", "DAPP") {}
 
     function mint(string memory _tokenURI) external returns (uint256) {
         tokenCount++;
--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,6 +1,25 @@
+import react, { useEffect, useState } from "react";
 import "./App.css";
+import { ethers } from "ethers";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  //Metamask login connect
+  useEffect(() => {
+    const web3Handler = async () => {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      setAccount(accounts[0]);
+      console.log("accounts", accounts);
+
+      //Get provider from metamask
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const singer = provider.getSigner();
+      console.log("singer", singer);
+    };
+
+    web3Handler();
+  }, []);
+
   return <div className="App">Maniknadan..</div>;
 }
 
